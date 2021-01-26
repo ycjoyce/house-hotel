84ce48b0f26a2aa02a0a1f69ca22fd49f5b8787f
Joyce <yeechiajoyce@gmail.com>
2021-01-26 15:58:26 +0800

    介接API

---
+++ src/assets/js/getData.js
@@ -0,0 +1,6 @@
+import fetch from '@src/assets/js/fetch';
+
+export const getAllRooms = (data) => fetch('https://challenge.thef2e.com/api/thef2e2019/stage6/rooms', data, 'GET');
+export const getRoomDetail = (id, data) => fetch(`https://challenge.thef2e.com/api/thef2e2019/stage6/room/${id}`, data, 'GET');
+export const reserveRoom = (id, data) => fetch(`https://challenge.thef2e.com/api/thef2e2019/stage6/room/${id}`, data, 'POST');
+export const clearAllReservation = (data) => fetch('https://challenge.thef2e.com/api/thef2e2019/stage6/rooms', data, 'DELETE');
